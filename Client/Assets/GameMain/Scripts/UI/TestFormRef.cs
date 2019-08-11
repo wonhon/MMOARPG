ddcd7715fcbb2dac6f9948844b32af4e5c28b415
wonhon <whang1108@163.com>
2019-08-11 21:15:30 +0800

    + 测试自动生成UI脚本功能

---
+++ Client/Assets/GameMain/Scripts/UI/TestFormRef.cs
@@ -0,0 +1,32 @@
+﻿//------------------------------------------------------------
+// 此文件由工具自动生成，请勿直接修改
+// 生成时间：2019-08-11-00:28:23
+//------------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SuperBiomass
+{
+   public partial class TestForm
+   {
+       [SerializeField]
+       private GameObject m_Container;
+
+       [SerializeField]
+       private Image m_Image;
+
+       [SerializeField]
+       private Text m_Text;
+
+       [SerializeField]
+       private Button m_Button;
+
+       [SerializeField]
+       private Toggle m_Toggle;
+
+       [SerializeField]
+       private Slider m_Slider;
+
+   }
+}
