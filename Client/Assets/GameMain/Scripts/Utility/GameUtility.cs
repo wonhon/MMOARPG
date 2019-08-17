c0ecf0e2c29e548d8fcbff28a6861b4a8b2833b9
wonhon <whang1108@163.com>
2019-08-18 00:48:46 +0800

    Add 游戏工具类

---
+++ Client/Assets/GameMain/Scripts/Utility/GameUtility.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//===================================================
+//作    者：WonHon
+//创建时间：2019-08-11-23:17:27
+//备    注：
+//===================================================
+
+namespace SuperBiomass
+{
+    public class GameUtility
+    {
+        public static void SetActive(GameObject gameObject, bool active)
+        {
+            if (active != gameObject.activeInHierarchy)
+            {
+                gameObject.SetActive(active);
+            }
+        }
+    }
+}
+
