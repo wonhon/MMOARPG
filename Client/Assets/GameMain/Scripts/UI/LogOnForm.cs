86dd4b7d00d4ecbf9c01d9a44623a22cdadb148d
wonhon <whang1108@163.com>
2019-08-18 15:29:34 +0800

    修复 生成UI脚本，成员变量名不能为_的bug

---
+++ Client/Assets/GameMain/Scripts/UI/LogOnForm.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+//===================================================
+//作    者：WonHon
+//创建时间：2019-08-11-22:55:42
+//备    注：登陆界面
+//===================================================
+
+namespace SuperBiomass
+{
+    public partial class LogOnForm : UGuiForm
+    {
+        private ProcedureLogOn m_ProcedureLogOn;
+
+        protected override void OnInit(object userData)
+        {
+            base.OnInit(userData);
+
+            m_LogOn.onClick.RemoveAllListeners();
+            m_LogOn.onClick.AddListener(OnLogOn);
+
+            m_ToReg.onClick.RemoveAllListeners();
+            m_ToReg.onClick.AddListener(OnToReg);
+        }
+
+        protected override void OnOpen(object userData)
+        {
+            base.OnOpen(userData);
+
+            m_ProcedureLogOn = userData as ProcedureLogOn;
+            if (m_ProcedureLogOn == null)
+            {
+                Log.Warning("ProcedureLogOn is null");
+                return;
+            }
+        }
+
+        private void OnLogOn()
+        {
+            m_ProcedureLogOn.SelectRole();
+        }
+
+        private void OnToReg()
+        {
+            m_ProcedureLogOn.SelectRole();
+        }
+    }
+}
---
+++ Client/Assets/GameMain/Scripts/UI/LogOnFormRef.cs
@@ -0,0 +1,29 @@
+﻿//------------------------------------------------------------
+// 此文件由工具自动生成，请勿直接修改
+// 生成时间：2019-08-18-13:41:36
+//------------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SuperBiomass
+{
+   public partial class LogOnForm
+   {
+       [SerializeField]
+       private Button m_Close;
+
+       [SerializeField]
+       private Button m_LogOn;
+
+       [SerializeField]
+       private Button m_ToReg;
+
+       [SerializeField]
+       private InputField m_Account;
+
+       [SerializeField]
+       private InputField m_Pwd;
+
+   }
+}
